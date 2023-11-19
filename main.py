9749fa98f82b2da0c57520628a091fdfaddbddfd
mishajw <mishajw@gmail.com>
2023-11-19 19:11:15 +0000

    Add batching; Add writing to disk.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 import math
+from pathlib import Path
+from typing import Callable
 from warnings import WarningMessage
 import seaborn as sns
 
@@ -28,6 +31,8 @@ SPARSITY = 0.999
 WEIGHT_DECAY = 1e-2
 N_BATCHES = 50_000
 N_LR_WARMUP_STEPS = 2_500
+BATCH_SIZE = 128
+
 
 class DDModel(nn.Module):
     def __init__(self, n_features: int, hidden_width: int) -> None:
@@ -36,32 +41,34 @@ class DDModel(nn.Module):
         self.biases = nn.Parameter(torch.empty(n_features))
         nn.init.xavier_uniform_(self.weights)
         nn.init.zeros_(self.biases)
-        
+
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         hidden = torch.matmul(x, self.weights)
         output = torch.relu(torch.matmul(hidden, self.weights.T) + self.biases)
         return hidden, output
-    
-@dataclass
-class TrainResults:
-    model_dict: dict[str, torch.Tensor]
-    inputs: torch.Tensor
-    weight_capacities: torch.Tensor
-    hidden_capacities: torch.Tensor
-    # inputs
-    hidden_dim: int
-    n_datapoints: int
+
+
+# @dataclass
+# class TrainResults:
+#     model_dict: dict[str, torch.Tensor]
+#     inputs: torch.Tensor
+#     weight_capacities: torch.Tensor
+#     hidden_capacities: torch.Tensor
+#     # inputs
+#     hidden_dim: int
+#     n_datapoints: int
+
 
 @torch.no_grad()
 def calculate_capacities(mat: Float[torch.Tensor, "objects width"]):
-        sizes = torch.norm(mat, dim=1) ** 2 # [objects]
-        normalized_mat = mat / torch.norm(mat, dim=1)[:, None]
-        cosine_sims = torch.matmul(normalized_mat, mat.T) ** 2 # [objects, objects]
-        total_cos_sims = torch.sum(cosine_sims, dim=1) # [objects]
-        return sizes / total_cos_sims
-    
-    
-def lr_schedule_creator(warmup_steps: int, total_steps: int):
+    sizes = torch.norm(mat, dim=1) ** 2  # [objects]
+    normalized_mat = mat / torch.norm(mat, dim=1)[:, None]
+    cosine_sims = torch.matmul(normalized_mat, mat.T) ** 2  # [objects, objects]
+    total_cos_sims = torch.sum(cosine_sims, dim=1)  # [objects]
+    return sizes / total_cos_sims
+
+
+def lr_schedule_creator(warmup_steps: int, total_steps: int) -> Callable[[int], float]:
     """
     warmup 2500 steps
     to 1e-3
@@ -69,10 +76,11 @@ def lr_schedule_creator(warmup_steps: int, total_steps: int):
     at 50,000 steps
     """
     assert warmup_steps < total_steps
-    def lr_schedule(epoch):
+
+    def lr_schedule(epoch: int) -> float:
         if epoch < warmup_steps:
             return epoch / warmup_steps
-        
+
         decay_len = total_steps - warmup_steps
         n_steps = epoch - warmup_steps
         return (1 + math.cos(math.pi * n_steps / decay_len)) / 2
@@ -80,43 +88,54 @@ def lr_schedule_creator(warmup_steps: int, total_steps: int):
     return lr_schedule
 
 
-def train_model(n_datapoints: int, hidden_dim: int, batch_size: int = 32) -> TrainResults:
+def train_model(n_datapoints: int, hidden_dim: int, output_dir: Path) -> None:
+    assert not output_dir.exists(), output_dir
+    output_dir.mkdir(exist_ok=True, parents=True)
+
     if ENABLE_WANDB:
         wandb.init(project="double_descent")
     model = DDModel(N_FEATURES, hidden_dim)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-3, weight_decay=WEIGHT_DECAY)
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=2e-3, weight_decay=WEIGHT_DECAY
+    )
     scheduler_fn = lr_schedule_creator(N_LR_WARMUP_STEPS, N_BATCHES)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler_fn)
     inputs = create_inputs(n_datapoints)
-    
-    loss_fn = nn.MSELoss()   
+
+    loss_fn = nn.MSELoss()
     for epoch in tqdm(range(N_BATCHES)):
         optimizer.zero_grad()
-        for i in range(0, n_datapoints, batch_size):
-            batch_inputs = inputs[i:i+batch_size]
+        for i in range(0, n_datapoints, BATCH_SIZE):
+            batch_inputs = inputs[i : i + BATCH_SIZE]
             hidden, output = model(batch_inputs)
             loss_multiplier = batch_inputs.shape[0] / inputs.shape[0]
             loss = loss_fn(output, batch_inputs) * loss_multiplier
             loss.backward()
         optimizer.step()
         scheduler.step()
-        
+
         if ENABLE_WANDB:
-            wandb.log({
-                "epoch": epoch,
-                "loss": loss.item(),
-                "lr": scheduler.get_last_lr()[0],
-                "weight_norm": torch.norm(model.weights).item(),
-                "bias_norm": torch.norm(model.biases).item(),
-                "hidden_norm": torch.norm(hidden).item(),
-            })
-    
-    hidden, _ = model(inputs)
-    weight_capacities = calculate_capacities(model.weights)
-    hidden_capacities = calculate_capacities(hidden)
-    model.eval()
-    model_state_dict = model.state_dict()
-    return TrainResults(model_state_dict, inputs.detach(), weight_capacities, hidden_capacities, hidden_dim, n_datapoints)
+            wandb.log(
+                {
+                    "epoch": epoch,
+                    "loss": loss.item(),
+                    "lr": scheduler.get_last_lr()[0],
+                    "weight_norm": torch.norm(model.weights).item(),
+                    "bias_norm": torch.norm(model.biases).item(),
+                    "hidden_norm": torch.norm(hidden).item(),
+                }
+            )
+
+    torch.save(model, output_dir / "model.pt")
+    torch.save(inputs, output_dir / "inputs.pt")
+    (output_dir / "config.json").write_text(
+        json.dumps(
+            dict(
+                n_datapoints=n_datapoints,
+                hidden_dim=hidden_dim,
+            )
+        )
+    )
 
 
 def create_inputs(n_datapoints: int) -> torch.Tensor:
@@ -130,7 +149,7 @@ def create_inputs(n_datapoints: int) -> torch.Tensor:
 
 def main():
     train_model(hidden_dim=2)
-    
-        
+
+
 if __name__ == "__main__":
     main()
